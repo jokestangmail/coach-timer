d3779bdabeb7e5f5c37dc74bd7bf04e989e8d398
jokestangmail <jokestan@gmail.com>
2025-09-24 15:00:26 -0400

    v3_5

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('ct-v3-2a').then(c=>c.addAll(['index.html','manifest.json','icon-192.png','icon-512.png','apple-touch-icon.png'])).then(()=>self.skipWaiting()));});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('ct-v3-5').then(c=>c.addAll(['index.html','manifest.json','icon-192.png','icon-512.png','apple-touch-icon.png'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
