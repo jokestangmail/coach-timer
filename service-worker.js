ade48a63086f45bc0a74a9e6458bc284ef8dd95f
jokestangmail <jokestan@gmail.com>
2025-09-24 12:04:44 -0400

    v3.1

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
-self.addEventListener('fetch',e=>{});
+
+  const CACHE = "coach-timer-pwa-v3-full";
+  const ASSETS = ["index.html","manifest.json","icon-192.png","icon-512.png","apple-touch-icon.png"];
+  self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    );
+  });
+  self.addEventListener("activate", (event) => { event.waitUntil(self.clients.claim()); });
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+  });
