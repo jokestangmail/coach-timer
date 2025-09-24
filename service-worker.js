8b110b602645d08c6c47ba3d2f3900db5b30ac9f
jokestangmail <jokestan@gmail.com>
2025-09-24 11:17:26 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-  const CACHE = "coach-timer-pwa-v1";
+  const CACHE = "coach-timer-pwa-v2";
   const ASSETS = ["index.html","manifest.json","icon-192.png","icon-512.png","apple-touch-icon.png"];
 
   self.addEventListener("install", (event) => {
